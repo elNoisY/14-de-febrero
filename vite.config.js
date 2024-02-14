896d22d87f653a3021da3b310b4cc4f962069110
elNoisY <159972832+elNoisY@users.noreply.github.com>
2024-02-13 20:40:36 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://martl7.github.io/February-Declaration/",
+  base: "https://elNoisY.github.io/14-de-febrero/",
 })
